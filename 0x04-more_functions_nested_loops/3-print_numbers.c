a600e9b4d08dd0ef9cac94cbefe514c82ebe54f2
kokeb19 <kokeby19@gmail.com>
2022-03-17 11:51:53 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - entry
+ *
+ * return: void
+ */
+void print_numbers(void)
+{
+	int number;
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
