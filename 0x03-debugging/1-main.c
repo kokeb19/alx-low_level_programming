45b556be17b131360fa1abecfd2e3fff3c3288e3
kokeb19 <kokeby19@gmail.com>
2022-03-17 14:51:05 -0700

    com

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+/**
+ * main - infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	int i=0;
+	/*while (i < 10)*/
+	/*{*/
+	/*_putchar(i);*/
+	/*}*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
