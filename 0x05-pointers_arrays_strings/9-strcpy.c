06bbb4d003f483fba524fc03d32cf0e4cc06df99
kokeb19 <kokeby19@gmail.com>
2022-03-22 07:47:34 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy
+ * @dst: destination
+ * @src: source
+ * Return: the pointer 
+ */
+char *_strcpy(char *dst, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dst + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dst);
+}
