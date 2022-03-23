991c4bf4dcb9bcc7d633c9dc35c214b2b395f39b
kokeb19 <kokeby19@gmail.com>
2022-03-23 12:58:22 -0700

    ad

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * string_toupper - change to uppercasae
  * @s: input
@@ -11,8 +12,8 @@ char *string_toupper(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if ((*(s + count) >= 97 && (*(s + count) <= 122))
-				*(s + count) = *(s + count) - 32;
+		if ((*(s + count) >= 'a' && (*(s + count) <= 'z'))
+				s[count] = s[count] - 32;
 				count++;
 	}
 
